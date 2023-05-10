f898a8fddce2cf2890e3a851a82cdf3b233bcd88
aliaz9 <alina.alvarez9@gmail.com>
2023-05-10 13:35:47 -0300

    Componente Productos

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Example from './Products'
+import Toys from './Products'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>All Products</h1>
-      <Example />
+    <div>
+     <Toys />
+     </div>
     </>
   )
 }
--- src/Products.jsx
+++ src/Products.jsx
@@ -5,6 +5,9 @@ import { useEffect, useState } from "react";
 import supabase from "./config/supabaseClient";
 
 
+
+
+
 // const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.VITE_SUPABASE_ANON_KEY);
 // const supabase = createClient("https://bgitqfphuplxykecaexv.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJnaXRxZnBodXBseHlrZWNhZXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyMDg2NTgsImV4cCI6MTk5ODc4NDY1OH0.Uzya4hqcBx7fEB9rwI7id3fZ9yqqIObzWdxRUHfqZkg");
 
@@ -30,7 +33,7 @@ import supabase from "./config/supabaseClient";
 //     // More products...
 //   ]
   
-  export default function Example() {
+  export default function Toys() {
 
     // const [products, setProducts] = useState([]);
 
@@ -73,19 +76,45 @@ useEffect( () => {
 }, [] );
 
     return (
-
       
       <div>
-        <h1>JUGUETES</h1>
+
+        <div className="bg-white">
+          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <h2 className="sr-only">Products</h2>
+
+            <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              {products.map((product) => (
+                <a key={product.id} href={product.href} className="group">
+                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg xl:aspect-h-8 xl:aspect-w-7">
+                    <img
+                      src={product.img}
+                      alt={product.title}
+                      className="h-full w-full object-cover object-center group-hover:opacity-75"
+                    />
+                  </div>
+                  <h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
+                  <p className="mt-1 text-lg font-medium text-gray-900">{"$"+product.price}</p>
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+
+
+
+
 
         {/* {fetchError && (<p>{fetchError}</p>)} */}
-         {products && (
+         {/* {products && (
           <div>
             {products.map(p => (
+              <div key={p.id}>
               <p>{p.title}</p>
+              </div>
             ))}
           </div>
-        )}
+        )} */}
 
       </div>
 
